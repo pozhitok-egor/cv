2b1c210f2de42d3284a36f611fb5a6f729549fb7
Egor Pozhitok <egor_pozhitok@epam.com>
2023-01-15 13:23:07 +0100

    feat: add udemy course, fix links and change style

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -34,8 +34,6 @@ module.exports = {
       ],
     }),
 
-    // Generates an HTML file from a template
-    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
       favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/index.html', // template file
